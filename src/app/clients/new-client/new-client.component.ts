a43d013d23f15dc6376d4e97cdd3021ec3d60512
Rodrigo Amorim <amorimrodrigo071@gmail.com>
2025-03-19 16:44:19 -0300

    customer registration page finished

--- src/app/clients/new-client/new-client.component.ts
+++ src/app/clients/new-client/new-client.component.ts
@@ -1,22 +1,49 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
 import { IClientService } from '../../services/api-client/clients/iclients.service';
-import { SERVICES_TOKEN } from '../../services/service.token';
 import { ClientsService } from '../../services/api-client/clients/clients.service';
+import { SERVICES_TOKEN } from '../../services/service.token';
+import { ClientModelForm } from '../client.models';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { ClientFormComponent } from "../components/client-form/client-form.component";
+import { ISnackBarManagerService } from '../../services/isnackbar-manager.service';
+import { SnackbarManagerService } from '../../services/snackbar-manager.service';
 
 @Component({
   selector: 'app-new-client',
-  imports: [],
+  imports: [ClientFormComponent],
   templateUrl: './new-client.component.html',
   styleUrl: './new-client.component.scss',
   providers: [
-    {
-      provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService
-    }
+    {provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService},
+    {provide: SERVICES_TOKEN.SNACKBAR, useClass: SnackbarManagerService}
   ]
 })
-export class NewClientComponent {
+export class NewClientComponent implements OnDestroy {
 
-  constructor(@Inject(SERVICES_TOKEN.HTTP.CLIENT) private readonly httpService: IClientService) { }
+private httpSubscription?: Subscription;
+
+  constructor(
+    @Inject(SERVICES_TOKEN.HTTP.CLIENT) private readonly httpService: IClientService,
+    @Inject(SERVICES_TOKEN.SNACKBAR) private readonly snackBarManager: ISnackBarManagerService,
+    private readonly router: Router
+  ) { }
+  
+  ngOnDestroy(): void {
+    if (this.httpSubscription){
+      this.httpSubscription.unsubscribe();
+    }
+  }
+
+  onSubmitClient(value: ClientModelForm) {
+    const {id, ...request} = value
+    this.httpSubscription = this.httpService.save(request).subscribe(_=>{
+      this.snackBarManager.show('Cliente salvo com sucesso!');
+      this.router.navigate(['/clients/list']);
+    })
+    
+  }
+
 
   
 }
---
+++ src/app/services/isnackbar-manager.service.ts
@@ -0,0 +1,5 @@
+export interface ISnackBarManagerService {
+
+    show(message: string, action?: string, duration?: number): void;
+
+}
